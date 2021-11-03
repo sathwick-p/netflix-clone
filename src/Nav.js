41efcbdb5e2cdc93b2373e9c26fbb4f1fa818af8
Sathwick <sathwick.p7@gmail.com>
2021-11-04 02:41:07 +0530

    edit

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Logo from "./Netflix_Logo_RGB.png";
-import Avatar from "./user.jpg";
+
 import "./Nav.css";
 function Nav() {
   const [show, handleShow] = useState(false);
@@ -21,7 +21,6 @@ function Nav() {
   return (
     <div className={`nav ${show && "nav__black"}`}>
       <img className="nav__logo" src={Logo} />
-      <img className="nav__avatar" src={Avatar} />
     </div>
   );
 }
